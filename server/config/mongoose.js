d379cc024666a3de7eb073331d4667dc2820a0a4
Anna Clawson <seadeer@gmail.com>
2016-04-22 15:38:46 -0700

    add debug to mongoose

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://localhost/polls')
+mongoose.connect('mongodb://localhost/questions');
+mongoose.set('debug', true);
 var models_path = path.join(__dirname, './../models');
 
 fs.readdirSync(models_path).forEach(function(file){
