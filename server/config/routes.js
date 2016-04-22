eb3281a41e4dbb7fb354b8ab4833c50743fdd6dc
Anna Clawson <seadeer@gmail.com>
2016-04-22 15:44:02 -0700

    add more routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -14,4 +14,24 @@ module.exports = function(app){
     app.post('/login', function(req, res){
         users.login(req, res);
     });
+
+    app.post('/questions/new', function(req, res){
+        questions.create(req, res);
+    });
+
+    app.get('/questions', function(req, res){
+        questions.index(req, res);
+    });
+
+    app.get('/questions/:id', function(req, res){
+        questions.show(req, res);
+    });
+
+    app.post('/answers/new', function(req, res){
+        answers.create(req, res);
+    })
+
+    app.post('/answers/:id', function(req, res){
+        answers.like(req, res)
+    })
 };
