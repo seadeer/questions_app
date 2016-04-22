21d2df311a926914a8fb1a237fb3aced5d43c3ce
Anna Clawson <seadeer@gmail.com>
2016-04-22 15:45:44 -0700

    add refs to questions and answer models

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var AnswerSchema = new mongoose.Schema({
-
     answer: String,
     details: String,
     likes: Number,
+    username: String,
+    _question: {type: Schema.Types.ObjectId, ref: 'Question'},
     _user: {type: Schema.Types.ObjectId, ref: 'User'}
 }, {timestamps: true})
 
--- server/models/question.js
+++ server/models/question.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 var QuestionSchema = new mongoose.Schema({
     question: String,
     description: String,
-    _user: {type: Schema.Types.ObjectId, ref: 'User'},
+    answers: [{type: Schema.Types.ObjectId, ref: 'Answer'}],
+    _user: {type: Schema.Types.ObjectId, ref: 'User'}
 }, {timestamps: true});
 
 var Question = mongoose.model('Question', QuestionSchema);
