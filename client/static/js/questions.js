d52450218c3cb0b68b83a3051d113c1feb5f6978
Anna Clawson <seadeer@gmail.com>
2016-04-22 15:43:14 -0700

    question factory and controller

--- client/static/js/questions.js
+++ client/static/js/questions.js
@@ -100,9 +100,83 @@ questionsApp.controller('usersController', function(userFactory, $location){
 
 
 //QUESTION FACTORY
+questionsApp.factory('questionFactory', function($http){
+    var factory = {};
+    factory.questions = {};
 
+    factory.index = function(callback){
+        $http.get('/questions').success(function(output){
+            factory.questions = output;
+            callback(factory.questions);
+        })
+    };
+
+    factory.create = function(question, callback){
+        console.log("Sending this off to db", question);
+        $http.post('/questions/new', question).success(function(output){
+            callback(output)
+        })
+    }
+
+    return factory;
+})
 //QUESTION CONTROLLER
+questionsApp.controller('questionsController', function(userFactory, questionFactory, $location){
+    var that = this;
+    this.questions = [];
+    this.user = userFactory.user();
+    this.errors = [];
 
+    this.index = function(){
+        questionFactory.index(function(data){
+            if(data){
+                that.questions = data;
+            }
+        });
+    };
+
+    this.index();
+
+    this.create = function(){
+        that.errors = [];
+        //Question validations
+        console.log(that.newQuestion);
+        if(typeof that.newQuestion.question !== 'undefined'){
+            if(that.newQuestion.question.length < 10){
+                that.errors.push("Question too short!")
+            }
+        }
+        else{
+            that.errors.push("Empty question!")
+        }
+        console.log("Error check: ", that.errors);
+        //If no errors, send off to factory
+        if(that.errors.length <= 0){
+            console.log("Creating new question, validations passed!")
+            var question = {
+                question: that.newQuestion.question,
+                description: that.newQuestion.description,
+                _user: that.user._id
+            };
+            console.log(question)
+            questionFactory.create(question, function(data){
+                console.log(data);
+                that.newQuestion = {};
+            });
+            $location.url('/home');
+            that.index();
+        }
+    };
+
+    this.logout = function(){
+        console.log(that.user);
+        userFactory.logout(function(){
+            $location.url('/login');
+            that.user = {};
+        });
+    };
+
+})
 //ANSWER FACTORY
 
 //ANSWER CONTROLLER
